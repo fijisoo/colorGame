5caa8cec180401c742d7998243ca8b5a9f43cbc1
Dominik <bendominik@gmail.com>
2018-01-31 14:10:42 +0100

    beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DndModule} from 'ng2-dnd';
 
 import { AppComponent } from './app.component';
 import { SingleSquareComponent } from './single-square/single-square.component';
+import { SquareColorComponent } from './square-color/square-color.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleSquareComponent
+    SingleSquareComponent,
+    SquareColorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/single-square/single-square.component.ts
+++ src/app/single-square/single-square.component.ts
@@ -16,5 +16,4 @@ export class SingleSquareComponent implements OnInit {
 
 
 
-
 }
