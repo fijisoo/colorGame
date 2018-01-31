28222a77865b4bf58d6c80e32e4f84f3a880d6b5
Dominik <bendominik@gmail.com>
2018-01-31 14:11:57 +0100

    [beta] added working new game button, working drag and drop functionality

---
+++ src/app/square-color/square-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square-color',
+  templateUrl: './square-color.component.html',
+  styleUrls: ['./square-color.component.css']
+})
+export class SquareColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
